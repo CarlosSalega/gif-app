66f21dc759e4bb35e08a97f5fff141669d36529a
CarlosSalega <carlosalfredosalega@gmail.com>
2023-07-08 21:22:16 -0200

    refactor: ♻️ barrels adjustment

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export { default as AddCategory } from "./AddCategory";
-export { default as GifGrid } from "./GifGrid";
-export { default as GifItem } from "./GifItem";
+export * from "./AddCategory";
+export * from "./GifGrid";
+export * from "./GifItem";
---
+++ src/helpers/index.ts
@@ -0,0 +1,2 @@
+export * from "./getGifs";
+export * from "./types";
